4cc04bd8b00195e0fdca3894381841fc507ca666
alperen <alperenkonuk93@gmail.com>
2025-02-19 12:58:11 +0300

    added darkMode option

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import {Route, Routes} from "react-router";
 import RegistrationPage from "./pages/RegistrationPage";
-import {Route, Router, Routes} from "react-router";
+import LoginPage from "./pages/LoginPage";
+import React, {useEffect, useState} from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    let savedMode = localStorage.getItem("displayMode");
+    if (!savedMode) {
+      const newMode = "dark";
+      setDarkMode(false);
+      localStorage.setItem("displayMode", savedMode);
+    }
+    setDarkMode(savedMode === 'dark');
+  }, []);
 
   return (
-     <RegistrationPage/>
+    <div className{`flex flex-col ${darkMode ? "dark" : ""}`}>
+      <Routes>
+        <Route path="/" element={<RegistrationPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+      </Routes>
+    </div>
   );
 }
 
